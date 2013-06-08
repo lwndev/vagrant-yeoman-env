5aafa69d4dd1be10510f1f86470a2aff512c2162
Leif Nelson <leif@lwndev.com>
2013-06-08 18:48:37 -0500

    Fix for #2

--- vagrant/manifests/default.pp
+++ vagrant/manifests/default.pp
@@ -15,29 +15,36 @@ class must-have {
 
   exec { 'install gem compass':
     command => '/opt/ruby/bin/gem install compass',
+    creates => "/opt/ruby/bin/compass",
     require => Exec["apt-get update 2"],
   }
 
   exec { 'install gem sass':
     command => '/opt/ruby/bin/gem install sass',
+    creates => "/opt/ruby/bin/sass",
     require => Exec["apt-get update 2"],
   }
 
   exec { 'install yeoman':
     command => '/usr/bin/npm install -g yo grunt-cli bower',
+    creates => [
+      '/usr/lib/node_modules/bower/bin/bower',
+      '/usr/lib/node_modules/yo/bin/yo',
+      '/usr/lib/node_modules/grunt-cli/bin/grunt'
+      ],
     require => [ Exec["apt-get update 2"], Package["nodejs"] ],
   }
 
   exec { 'install webapp generator':
     command => '/usr/bin/npm install -g generator-webapp',
+    creates => '/usr/lib/node_modules',
     require => Exec["install yeoman"],
   }
 
   package { ["vim",
              "bash",
              "nodejs",
-             "git-core",
-             "apache2"]:
+             "git-core"]:
     ensure => present,
     require => Exec["apt-get update 2"],
   }
